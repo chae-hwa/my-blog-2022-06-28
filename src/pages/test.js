a520e53714de76c4de72e0af497b0e81e08b001d
chae-hwa <ihj0519@naver.com>
2022-06-28 19:23:33 +0900

    테스트 페이지 추가

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <main>
+      <title>Test Page</title>
+      <h1>Test Page</h1>
+    </main>
+  );
+};
+
+export default TestPage;
